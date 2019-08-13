3a042785a527d92e21fbe6ee88e4900ebf0e7f9d
Jason Bock <jason@jasonbock.net>
2019-08-13 12:07:54 -0500

    Added a StackTraces example to show diff between .NET Core 2.0 and 2.2

---
+++ Exceptions/Exceptions.StackTraces/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Exceptions.StackTraces
+{
+	class Program
+	{
+		static async Task Main() => await Program.FirstLevelAsync();
+
+		private static async Task FirstLevelAsync() => await Program.SecondLevelAsync();
+
+		private static async Task SecondLevelAsync() => await Program.ThirdLevelAsync();
+
+		private static async Task ThirdLevelAsync() => await Program.IterateAsync();
+
+		private static async Task IterateAsync()
+		{
+			var values = new Dictionary<int, int>
+			{
+				{ 0, 1 },
+				{ 1, 2 },
+				{ 2, 3 },
+				{ 4, 5 }
+			};
+
+			for(var i = 0; i < values.Count; i++)
+			{
+				await Task.Delay(50);
+				await Console.Out.WriteLineAsync($"{i}, {values[i]}");
+			}
+		}
+	}
+}
